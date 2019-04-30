57159d5906723617e224ce6935bf8f3c7cfec45b
ПК <ilya.nakhaichuk@gmail.com>
2019-04-30 14:37:40 +0300

    Two new tasks

--- src/MetroTest/app/Metro.java
+++ src/MetroTest/app/Metro.java
@@ -5,12 +5,6 @@ import java.util.ArrayList;
 class Metro {
 
     public static int countPassengers(ArrayList<int[]> stops) {
-        int result=0;
-        for(int i=0;i<stops.size();i++){
-            if(stops.get( i )[0]<0 || stops.get( i )[1]<0){return 0;}
-                result +=stops.get( i )[0];
-                result-=stops.get( i )[1];
-        }
-        return result;
+             return stops.stream().mapToInt( value -> value[0]-value[1]).sum();
     }
 }
