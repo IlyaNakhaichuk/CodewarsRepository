267bc4fcf05ddf15918babe6b0912a6134f27f25
ПК <ilya.nakhaichuk@gmail.com>
2019-05-02 15:57:31 +0300

    add ArithmeticFunction

---
+++ src/DirReduction/app/DirReduction.java
@@ -0,0 +1,9 @@
+package DirReduction.app;
+
+/*public class DirReduction{
+    public static String[] dirReduc(String[] arr) {
+
+        return new String[]{new String( String.valueOf( arr ))};
+    }
+}
+*/
