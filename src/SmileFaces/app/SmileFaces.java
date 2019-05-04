cc498dde486d4d579a6f9730e0f50600361a967b
ПК <ilya.nakhaichuk@gmail.com>
2019-05-04 12:49:38 +0300

    add SmileFaces

---
+++ src/SmileFaces/app/SmileFaces.java
@@ -0,0 +1,10 @@
+package SmileFaces.app;
+
+import java.util.*;
+
+public class SmileFaces {
+
+    public static int countSmileys(List<String> arr) {
+        return (int)arr.stream().filter( x->x.matches( "[:;][-~]?[)D]" )).count();
+    }
+}
