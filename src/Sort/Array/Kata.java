cc6ec097cc0b73aa1435e93ed3595d5664ca72a7
ПК <ilya.nakhaichuk@gmail.com>
2019-05-31 16:18:31 +0300

    add Solution(6 kurs) other decision

--- src/Sort/Array/Kata.java
+++ src/Sort/Array/Kata.java
@@ -2,30 +2,13 @@ package Sort.Array;
 
 public class Kata {
     public static int[] sortArray(int[] array) {
-        int mass=0;
-        for(int a=0;a<array.length;a++){if(array[a]%2!=0)mass++;}
-        int [] arrayNechet=new int[mass];
-        int count=0;
-        for(int i=0;i<array.length;i++){
-            if(array[i]%2!=0){
-                arrayNechet[count]=array[i];
-                count++;
-            }
-        }
-        for(int i=arrayNechet.length-1;i>0;i--){
-            for(int j=0;j<i;j++){
-                if(arrayNechet[j]>arrayNechet[j+1]) {
-                        int temp = arrayNechet[j];
-                        arrayNechet[j] = arrayNechet[j + 1];
-                        arrayNechet[j + 1] = temp;
+        for(int i=0 ; i<array.length-1 ; i++){
+            for(int j=i+1; j<array.length ; j++){
+                if(array[i]>array[j] && array[i]%2 == 1 && array[j]%2 == 1){
+                    int aux=array[i];
+                    array[i]=array[j];
+                    array[j]=aux;
                 }
-            }
-        }
-        int num=0;
-        for(int i=0;i<array.length;i++){
-            if(array[i]%2!=0){
-                array[i]=arrayNechet[num];
-                num++;
             }
         }
         return array;
