ad6581701e8f26b85d8286dc751732b0d1134e14
ПК <ilya.nakhaichuk@gmail.com>
2019-06-04 11:02:08 +0300

    upgrade EncodeSimple (6 kurs) Map<> ---- HashMap<>()

--- src/EncodeSimple/EncodeSimpleTest.java
+++ src/EncodeSimple/EncodeSimpleTest.java
@@ -1,18 +1,50 @@
 package EncodeSimple;
-
 import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class EncodeSimpleTest {
 
-    Leetspeak myEncoder = new Leetspeak();
+    @Test
+    public void fullTest(){
+        Encoder myEncoder = null;
+        try {
+            myEncoder = new Leetspeak();
+        } catch(Exception e) {
+            fail("Could not initialize the encoder properly");
+        }
+        assertNotNull("Encoder is null", myEncoder);
+        assertTrue("Empty string should return empty string", myEncoder.encode("").equals(""));
+        assertTrue("Null string should return empty string", myEncoder.encode(null).equals(""));
+        assertTrue("Failed simple encoding", myEncoder.encode("abcd").equals("4bcd"));
+        assertTrue("Failed uppercase encoding", myEncoder.encode("EFGH").equals("3FGH"));
+        assertTrue("Failed mixed case encoding", myEncoder.encode("Leetspeak").equals("133tsp34k"));
+        assertTrue("Failed complex encoding", myEncoder.encode("3e").equals("33"));
+        assertTrue("Failed complex uppercase encoding", myEncoder.encode("109ZUI").equals("109Z(_)I"));
+        assertTrue("Failed complex mixed case encoding", myEncoder.encode("^abcD").equals("^4bcD"));
+        assertTrue("Failed special encoding", myEncoder.encode("mVw").equals("/^^\\Vw"));
+        assertTrue("Failed special letters", myEncoder.encode(" ab cd ef gh ij kl mn op qr st uv wx yz\t\n\r\u2202 AB CD EF GH IJ KL MN OP QR ST UV WX YZ\t\r ").equals(" 4b cd 3f gh ij k1 /^^\\n 0p qr st (_)v wx yz\t\n\r\u2202 4B CD 3F GH IJ K1 /^^\\N 0P QR ST (_)V WX YZ\t\r "));
+    }
+
+    public static char[] alphaNum = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789    ".toCharArray();
 
     @Test
-    public void simpleTest(){
-        assertTrue("empty string", myEncoder.encode("").equals(""));
-    }
-    @Test
-    public void simlpeTest2(){
-        assertTrue("abcdef string", myEncoder.encode("abcdef").equals("4bcd3f"));
+    public void randomTests() {
+        Encoder myEncoder = null;
+        try {
+            myEncoder = new Leetspeak();
+        } catch(Exception e) {
+            fail("Could not initialize the encoder properly");
+        }
+        for (int trial = 1; trial <= 100; trial++) {
+            StringBuilder sb = new StringBuilder();
+            for (int length = (int)(Math.random() * 50); length > 0; length--)
+                sb.append(alphaNum[(int)(Math.random() * alphaNum.length)]);
+            String source   = sb.toString(),
+                    expected = source.replaceAll("(?i)a", "4").replaceAll("(?i)e", "3").
+                            replaceAll("(?i)l", "1").replaceAll("(?i)m", "/^^\\\\").
+                            replaceAll("(?i)o", "0").replaceAll("(?i)u", "(_)");
+            System.out.format("Trial #%d: \"%s\"\n", trial, source);
+            assertEquals(expected, myEncoder.encode(source));
+        }
     }
 }
--- src/EncodeSimple/Leetspeak.java
+++ src/EncodeSimple/Leetspeak.java
@@ -1,12 +1,28 @@
 package EncodeSimple;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Leetspeak extends Encoder {
 
     public String encode(String source){
-        // TODO - Encode the source string into a 133tSp34k string
-        String str = source.replaceAll( "(?i)a","4" ).replaceAll( "(?i)e", "3" ).replaceAll( "(?i)l","1" ).replaceAll("(?i)m","/^^\\\\").replaceAll( "(?i)o","0" ).replaceAll( "(?i)u","(_)" );
-        return str;
+        if(source == null || source == ""){return "";}
+
+        Map<Character,String> map = new HashMap<>(  );
+        map.put('a', "4");
+        map.put('e', "3");
+        map.put('l', "1");
+        map.put('m', "/^^\\");
+        map.put('o', "0");
+        map.put('u', "(_)");
+        StringBuilder out = new StringBuilder(  );
+        for(char x : source.toCharArray()){
+            String temp = map.get( Character.toLowerCase( x ) );
+            if(temp == null) temp = Character.toString( x );
+            out.append( temp );
+        }
+        return out.toString();
     }
 
 }
