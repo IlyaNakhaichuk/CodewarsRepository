1b89193b5b3c3a0f28d825fdef96a8752019dd56
ПК <ilya.nakhaichuk@gmail.com>
2019-06-13 13:48:43 +0300

    update Asum.app (6 kurs)

--- src/Asum/app/ASum.java
+++ src/Asum/app/ASum.java
@@ -5,7 +5,7 @@ public class ASum {
     public static long findNb(long m) {
         long resultOfExponentiation = 0;
         long reisedToNumber = 0;
-        while (resultOfExponentiation < m) {
+        for(;resultOfExponentiation < m;) {
             resultOfExponentiation += (long) Math.pow( ++reisedToNumber, 3 );
         }
         return (m==resultOfExponentiation) ? reisedToNumber : -1;
