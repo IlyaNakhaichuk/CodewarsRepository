41889812a85eda82b1aa9a119d6cf14c1bcf1f92
ПК <ilya.nakhaichuk@gmail.com>
2019-06-14 10:50:17 +0300

    add Persistent Bugger (6 kurs) use do while

---
+++ src/by/company/persistentBugger/Persist.java
@@ -0,0 +1,33 @@
+package by.company.persistentBugger;
+
+
+class Persist {
+    public static int persistence(long n) {
+        if (String.valueOf( n ).length()==1){
+            return 0;
+        }
+        int numberOfRepetitions=0;
+        do{
+            int [] convertidNunber = conversionNumber( n );
+            n=1;
+            for(int i = 0; i<convertidNunber.length;i++){
+                n*=convertidNunber[i];
+            }
+            numberOfRepetitions++;
+            if (String.valueOf( n ).length()>1){
+                continue;
+            }else {
+                return numberOfRepetitions;
+            }
+            }while (true);
+    }
+
+    public static int [] conversionNumber(long n){
+        String [] divisionNumbeToString =  String.valueOf( n ).split( "" );
+        int [] stringToNumber = new int [divisionNumbeToString.length];
+        for (int i = 0; i <divisionNumbeToString.length ; i++) {
+            stringToNumber[i]= Integer.parseInt( divisionNumbeToString[i]);
+        }
+        return stringToNumber;
+    }
+}
